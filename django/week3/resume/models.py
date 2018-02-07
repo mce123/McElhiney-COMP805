6b826efb8ec362a20eaa9ace53508b3d11665cae
mce123 <pmcelhiney@mce123.com>
2018-02-06 20:03:57 -0500

    Lab 3 Django - Models.py

--- django/week3/resume/models.py
+++ django/week3/resume/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Education(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    location = models.CharField(max_length=255, null=False, blank=False)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+class Experience(models.Model):
+    institution_name = models.CharField(max_length=255, null=False, blank=False)
+    location = models.CharField(max_length=255, null=False, blank=False)
+    degree = models.CharField(max_length=255, null=False, blank=False)
+    major = models.CharField(max_length=255, null=False, blank=False)
+    gpa = models.FloatField()
+
+    def __str__(self):
+        return self.institution_name
+
+    class Meta:
+        ordering = ['institution_name']
